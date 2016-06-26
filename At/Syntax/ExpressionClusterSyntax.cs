99f9a4b3a2b90d30ee1b030eae85f25faa9fcff7
Mark Cidade <marxidad@gmail.com>
2016-06-26 01:44:41 -0400

    (ExpressionClusterSyntax)

--- At/Syntax/ExpressionClusterSyntax.cs
+++ At/Syntax/ExpressionClusterSyntax.cs
@@ -1,6 +1,11 @@
-﻿namespace At.Syntax
+﻿using System.Collections.Generic;
+
+namespace At.Syntax
 {
-    internal class ExpressionClusterSyntax
+public class ExpressionClusterSyntax : AtSyntaxNode
+{
+    internal ExpressionClusterSyntax(IEnumerable<AtSyntaxNode> nodes, IEnumerable<AtDiagnostic> diagnostics) : base(nodes,diagnostics)
     {
     }
+}
 }
